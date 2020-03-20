b75880b4ed61d7d389af0c6f3d86434ad3ed1eaf
AzzBar <44644802+AzzBar@users.noreply.github.com>
2020-03-19 20:18:29 -0400

    Added an output change

---
+++ SalesTool/src/demo/salestool/Main.java
@@ -0,0 +1,19 @@
+package demo.salestool;
+
+public class Main {
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("Hello HAPPY Sales People!");
+		System.out.println("This App Shows Sales DATA!");
+		
+		System.out.println("Test 1");
+	}
+
+}
